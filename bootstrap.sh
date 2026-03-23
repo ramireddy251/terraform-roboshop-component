93ea05114dac59a21848b62a851faced6cfa0784
ram <ramireddy251@gmail.com>
2026-03-23 15:13:14 +0530

    terraform roboshop components

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+component=$1
+environment=$2
+app_version=$3
+dnf install ansible -y
+
+cd /home/ec2-user
+git clone https://github.com/ramireddy251/ansible-roboshop-roles-tf.git
+
+cd ansible-roboshop-roles-tf
+git pull
+ansible-playbook -e component=$component -e env=$environment -e app_version=$app_version roboshop.yml
+
